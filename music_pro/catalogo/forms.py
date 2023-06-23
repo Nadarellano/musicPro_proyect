41eb523b415b60740b97e96e1451aad6022df16b
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2023-06-23 19:13:06 -0400

    registrar usuario

--- music_pro/catalogo/forms.py
+++ music_pro/catalogo/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Contacto, Producto
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactoForm(forms.ModelForm):
 
@@ -15,3 +17,9 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = '__all__'
+
+class CustomUserCreationForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ['username', "first_name", "last_name", "email", "password1", "password2"]
