b34469731aa5d81600924828f10ced116a1dbf52
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2023-06-27 01:29:41 -0400

    tabla bodega

--- music_pro/bodega/models.py
+++ music_pro/bodega/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from catalogo.models import *  # Importa todos los modelos
 
-# Create your models here.
+class Inventario_Bodega(models.Model):
+    nombre_producto = models.CharField(max_length=50)
+    stock = models.IntegerField()
+    producto = models.ForeignKey(Producto, on_delete=models.DO_NOTHING)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre_producto
