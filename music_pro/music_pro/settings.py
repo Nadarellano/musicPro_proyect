f8e25078293651ecf1a650f36b6b23e79b53ca82
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2023-07-05 02:16:10 -0400

    Update app

--- music_pro/music_pro/settings.py
+++ music_pro/music_pro/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ventas',
     'bodega',
     'catalogo',
     'django.contrib.humanize',
